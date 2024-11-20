bacf3ea1a010228e835063cdfabf74fece3a2a07
Arthur <arthur.bartczak@epitech.eu>
2024-11-20 15:24:27 +0100

    javadoc name selection

--- src/entity/Warrior.java
+++ src/entity/Warrior.java
@@ -134,24 +134,7 @@ public class Warrior extends Player {
 
     //Change attackRangeDirection
 
-    @Override
-    public void update(ArrayList<Monster> monsters, int screenX, int screenY) {
-        super.update(monsters, screenX, screenY);
-        attackRangeDirection();
 
-        if (attackCooldown > 0) {
-            attackCooldown--;
-        }
-
-        if (isAttacking && attackCooldown == 0) {
-            for (Monster monster : monsters) {
-                if (checkRange(monster, screenX, screenY)) {
-                    attack(monster);
-                    attackCooldown = attackCooldownMax;
-                }
-            }
-        }
-    }
 
     /**
      * Updates the attack range based on the warrior's current direction.
@@ -187,15 +170,14 @@ public class Warrior extends Player {
         }
     }
 
-=
+
     /**
      * Draws the warrior on the screen, including animations, attack ranges, and solid areas.
      *
-     * @param g2 The Graphics2D object used for drawing.
      * @param screenX The X position of the warrior on the screen.
      * @param screenY The Y position of the warrior on the screen.
      */
-=
+
     @Override
     public void update(ArrayList<Monster> monsters, int screenX, int screenY){;
         super.update(monsters, screenX, screenY);
@@ -307,7 +289,7 @@ public class Warrior extends Player {
         g2.setColor(Color.WHITE);
         g2.drawString("HP: " + hp, screenX +10, screenY -10);
 
-    }
+
 
 
         // Draw attack range
